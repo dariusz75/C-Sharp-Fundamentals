bc72116c5c9d1d22a960ba843cc004c0695834ad
DariuszEbsta <dariuszfranczak@ebsta.com>
2019-11-11 23:21:25 +0000

    variables

---
+++ PrimitiveDataTypes/1.Variables/EntryPoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _1.Variables
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            int age = 26;
+            string name = "Tom";
+            string space = " ";
+
+            Console.WriteLine(name + space + "is " + age + ".");
+        }
+    }
+}
