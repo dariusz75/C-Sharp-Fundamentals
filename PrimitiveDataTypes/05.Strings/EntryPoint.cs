5c83a90ebff7037875d84fdbc9d03417f52a7117
DariuszEbsta <dariuszfranczak@ebsta.com>
2019-11-18 12:43:56 +0000

    trings completed

---
+++ PrimitiveDataTypes/05.Strings/EntryPoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Strings
+{
+    class EntryPoint
+    {
+        static void Main()
+        {
+            string username = "Dariusz";
+
+            System.Console.WriteLine(username[0]);
+            System.Console.WriteLine(username[6]);
+        }
+    }
+}
