22d1a68d634b0f91b8cead807ac12228f829feae
DariuszEbsta <dariuszfranczak@ebsta.com>
2019-11-25 09:55:55 +0000

    'swithc'

---
+++ PrimitiveDataTypes/10.Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Switch
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine($"Please choose one of the folloing units: mm, cm or m");
+            string unitsGiven = Console.ReadLine();
+
+            switch (unitsGiven)
+            {
+                case "mm":
+                    Console.WriteLine($"The units are milimeters");
+                    break;
+                case "cm":
+                    Console.WriteLine($"The units are centimeters");
+                    break;
+                case "m":
+                    Console.WriteLine($"The units are meters");
+                    break;
+                default:
+                    Console.WriteLine($"The chosen units are not correct");
+                    break;
+            }
+        }
+    }
+}
